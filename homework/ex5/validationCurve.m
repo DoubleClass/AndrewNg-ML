72c2fb1ccabe6771133f26463ef555930ddadffe
walter <tjcxs136720@163.com>
2020-03-15 21:18:25 +0800

    完成ex5后半部分

--- homework/ex5/validationCurve.m
+++ homework/ex5/validationCurve.m
@@ -1,3 +1,6 @@
+
+
+
 function [lambda_vec, error_train, error_val] = ...
     validationCurve(X, y, Xval, yval)
 %VALIDATIONCURVE Generate the train and validation errors needed to
@@ -39,10 +42,12 @@ error_val = zeros(length(lambda_vec), 1);
 %
 %
 
-
-
-
-
+for i = 1 : length(lambda_vec)
+    lambda = lambda_vec(i);
+    theta = trainLinearReg(X, y, lambda);
+    error_train(i) = linearRegCostFunction(X, y, theta, 0);
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
 
 
